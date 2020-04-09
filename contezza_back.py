e6ac616c746686bc08284ca54df4d6cebb4b0a3c
Rachit <rachit1405@gmail.com>
2020-04-10 01:38:20 +0530

    Update contezza_back.py

--- contezza_back.py
+++ contezza_back.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from email.message import EmailMessage
 import smtplib
 
 app = Flask(__name__)
@@ -13,12 +14,32 @@ class Rest_cont(Resource):
 
     def post(self):
         json = request.get_json()
-        server = smtplib.SMTP('smtp.gmail.com:587')
+              
+        to = json['email']
+        From = 'richa@contezzaconsultants.com'
+        server = smtplib.SMTP_SSL('mail.contezzaconsultants.com',465)
         server.ehlo()
-        server.starttls()
-        server.login('rachit1405@gmail.com','heisenberg321')
-        message= 'Subject: {}\n\n{}'.format('from:'+json['name']+' number:'+json['number'],json['text'])
-        server.sendmail('rachit1405@gmail.com',json['email'],message)
+        #server.starttls()
+        server.login('conteyf9','Manu@@2008')
+        
+        email_to = EmailMessage()
+        body_to = "Thanks for contacting. We will get back to you soon"  
+        email_to.set_content(body_to,subtype="html")
+        
+        email_to['From'] = From
+        email_to['To'] = to
+        email_to['Subject'] = "Contezza Consultants"
+        server.send_message(email_to)
+        
+        email_from = EmailMessage()
+        body_from = json['text']
+        email_from.set_content(body_from,subtype="html")
+        
+        email_from['From'] = From
+        email_from['To'] = From
+        email_from['Subject'] = "Query from: "+json['name']+" number: "+json['number']
+        server.send_message(email_from)
+        
         server.quit()
         return json
 
